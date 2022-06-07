592611284e6a35bad0aad961678244666522b9fa
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2022-06-07 17:03:35 +0200

    Update buttons.dart

--- calc/lib/buttons.dart
+++ calc/lib/buttons.dart
@@ -9,14 +9,17 @@ class MyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Container(
-        color: color,
-        child: Center(
-          child: Text(
-            buttonText,
-            style: TextStyle(color: textColor),
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          color: color,
+          child: Center(
+            child: Text(
+              buttonText,
+              style: TextStyle(color: textColor),
+            ),
           ),
         ),
       ),
