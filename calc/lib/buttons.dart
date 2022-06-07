a7d80db392210a4b335270bfb6f9a64c5b6ef568
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2022-06-07 16:57:33 +0200

    Update buttons.dart

--- calc/lib/buttons.dart
+++ calc/lib/buttons.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  final color;
-  final textColor;
+  dynamic color;
+  dynamic textColor;
   String buttonText;
 
-  MyButton(this.color, this.buttonText, this.textColor)
+  MyButton(this.color, this.buttonText, this.textColor);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Container(),
+    );
   }
 }
