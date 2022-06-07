bda649d0c1fed72cb2a6623f51298502118f9a12
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2022-06-07 16:35:13 +0200

    Update main.dart

--- calc/lib/main.dart
+++ calc/lib/main.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      
-    )
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculator'),
+      ),
+    );
   }
 }
